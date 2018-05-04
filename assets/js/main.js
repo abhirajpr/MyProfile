ec357cbe94810d9003eea386a77528b59ae4980f
Abhiraj P. Radhakrishnan <abhirajpr@suyatitech.local>
2018-05-04 15:32:23 +0530

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ jQuery(document).ready(function($) {
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "abhiraj", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "abhirajpr", selector: "#ghfeed" });
 
 
 });
